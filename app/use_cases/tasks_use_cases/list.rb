89b38c3b90e9941ddf67dfb34ef572bc0863fd8c
s-arikawa <arikawa@i3-systems.com>
2021-09-26 23:16:29 +0900

    rubocop auto correct

--- app/use_cases/tasks_use_cases/list.rb
+++ app/use_cases/tasks_use_cases/list.rb
@@ -4,7 +4,7 @@ module TasksUseCases
   # タスクの一覧を検索する。
   class List < ApplicationUseCase
     # @param [Hash] params
-    def all_tasks(params = {})
+    def all_tasks(_params = {})
       Task.all
     end
   end
